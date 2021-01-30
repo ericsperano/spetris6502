ffb23bddab1402a83a72c053e8b73131a2079b1f
Eric Sperano <eric.sperano@gmail.com>
2021-01-29 19:53:34 -0800

    high score

--- Spetris.Score.s
+++ Spetris.Score.s
@@ -1,29 +1,55 @@
 ***
 ***
 ***
-IncScore        lda LinesCount
-                asl ; multiply by 2
+IncScore        lda LinesCount                  ; y will be the index for the points table
+                asl                             ; multiply linescount by 2 because each entry is 2 bytes
                 tay
-                iny ; lo byte is in +1
-                ldx #3  ; 4 bytes - 1
+                iny                             ; lo byte is in +1
+                ldx #3                          ; start index for ScoreBCD
                 clc
-                sed
-                lda ScoreBCD,x
-                adc (PTR_Points),y
-                sta ScoreBCD,x
-                dex
+                sed                             ; set decimal mode
+                lda ScoreBCD,x                  ; load lowest byte of score
+                adc (PTR_Points),y              ; add lo byte of points to a
+                sta ScoreBCD,x                  ; store in score
+                dex                             ; decrement indexes
                 dey
+                lda ScoreBCD,x                  ; load 2nd lowest byte of score
+                adc (PTR_Points),y              ; add hi byte of points to a
+                sta ScoreBCD,x                  ; store in score
+                dex                             ; decrement index
                 lda ScoreBCD,x
-                adc (PTR_Points),y
-                sta ScoreBCD,x
-                dex
-isLoop          lda ScoreBCD,x
                 adc #0
                 sta ScoreBCD,x
                 dex
-                bne isLoop
-                cld ; binary mode
-                rts
+                lda ScoreBCD,x
+                adc #0
+                sta ScoreBCD,x
+                cld                             ; binary mode
+                * 32 bits comparisons of Score and HighScore
+                ldx #0
+                lda HighScoreBCD,x
+                cmp ScoreBCD,x
+                bcc updateScore                  ; high score < score
+                inx
+                lda HighScoreBCD,x
+                cmp ScoreBCD,x
+                bcc updateScore                  ; high score < score
+                inx
+                lda HighScoreBCD,x
+                cmp ScoreBCD,x
+                bcc updateScore                  ; high score < score
+                inx
+                lda HighScoreBCD,x
+                cmp ScoreBCD,x
+                bcc updateScore                  ; high score < score
+                jmp endCmpScore
+updateScore     ldx #0
+updateScoreL    lda ScoreBCD,x
+                sta HighScoreBCD,x
+                inx
+                cpx #4
+                bne updateScoreL
+endCmpScore     rts
 ***
 ***
 ***
